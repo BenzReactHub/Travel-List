56d1fa58fcccf062336ceadd288ed87e47448a10
lenrich <lenrich1403@gmail.com>
2023-11-01 17:29:30 +0800

    9. Calculating Statistics as Derived State

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ export default function App() {
     <div className="app">
       <Logo />
       <Form setItems={setItems} />
-      <PackingList items={items} setItems={setItems}/>
-      <Stats />
+      <PackingList items={items} setItems={setItems} />
+      <Stats items={items} />
     </div>
   );
 }
@@ -68,7 +68,7 @@ function PackingList({ items, setItems }) {
     <div className="list">
       <ul style={{}}>
         {items.map((item) => (
-          <Item key={item.id} item={item} setItems={setItems}/>
+          <Item key={item.id} item={item} setItems={setItems} />
         ))}
       </ul>
     </div>
@@ -76,26 +76,62 @@ function PackingList({ items, setItems }) {
 }
 
 function Item({ item, setItems }) {
-
   function deleteItem(id) {
-    setItems((prevItems)=> prevItems.filter((item)=> item.id !== id))
+    setItems((prevItems) => prevItems.filter((item) => item.id !== id));
+  }
+
+  function setItemPackStatus(id, status) {
+    // console.log(id, status)
+    setItems((prevItems) =>
+      prevItems.filter((item) => {
+        if (item.id === id) {
+          item.packed = status;
+        }
+        return item;
+      }),
+    );
   }
 
   return (
     <li>
+      <input
+        type="checkbox"
+        checked={item.packed}
+        onChange={(e) => {
+          setItemPackStatus(item.id, e.target.checked);
+        }}
+      />
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button onClick={()=>{deleteItem(item.id)}}>❌</button>
+      <button
+        onClick={() => {
+          deleteItem(item.id);
+        }}
+      >
+        ❌
+      </button>
     </li>
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding some items to your packing list 🛩</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
       <em>
-        (¬‿¬) You have X items on your list, and you already packed X (X%)
+        {percentage === 100
+          ? "You got everything! Ready to go "
+          : `(¬‿¬) You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
       </em>
     </footer>
   );
